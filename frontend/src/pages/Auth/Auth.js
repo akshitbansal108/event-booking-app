d0ed9b3fe00f2b08205222cfb5def75f02713274
akshitbansal-bst <akakshitbansal@gmail.com>
2020-09-20 10:26:29 +0530

    login signup added, error handling in resolvers

--- frontend/src/pages/Auth/Auth.js
+++ frontend/src/pages/Auth/Auth.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 
-import './Auth.css';
+import "./Auth.css";
 
 class AuthPage extends Component {
+  state = {
+    isLoggedIn: true,
+  };
+
   constructor(props) {
     super(props);
     this.emailElement = React.createRef();
     this.passwordElement = React.createRef();
   }
 
-  submit = () => {
+  switchHandler = () => {
+    this.setState((prevState) => {
+      return { isLoggedIn: !prevState.isLoggedIn };
+    });
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
     const email = this.emailElement.current.value;
     const password = this.passwordElement.current.value;
 
@@ -17,12 +28,55 @@ class AuthPage extends Component {
       return;
     }
 
-    console.log(email, password);
-  }
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!this.state.isLoggedIn) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    fetch("http://localhost:5000/home", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
-      <form className="auth-form" onSubmit={this.submit}>
+      <form className="auth-form" onSubmit={this.submitHandler}>
         <div className="form-input">
           <label htmlFor="email">Email</label>
           <input type="email" id="email" ref={this.emailElement} />
@@ -32,8 +86,12 @@ class AuthPage extends Component {
           <input type="password" id="password" ref={this.passwordElement} />
         </div>
         <div className="form-actions">
-          <button type="submit">Submit</button>
-          <button type="button">Signup</button>
+          <button type="submit">
+            {this.state.isLoggedIn ? "Login" : "Signup"}
+          </button>
+          <button type="button" onClick={this.switchHandler}>
+            Switch to {!this.state.isLoggedIn ? "Login" : "Signup"}
+          </button>
         </div>
       </form>
     );
