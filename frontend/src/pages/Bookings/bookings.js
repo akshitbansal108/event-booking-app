d89c428fd2ed2b34f8e5afcd358c7ab5f575063d
akshitbansal-bst <akakshitbansal@gmail.com>
2020-09-24 09:50:34 +0530

    bookings page added, fixes in events page

--- frontend/src/pages/Bookings/bookings.js
+++ frontend/src/pages/Bookings/bookings.js
@@ -1,8 +1,81 @@
 import React, { Component } from "react";
+import Spinner from "../../components/spinner/spinner";
+
+import AuthContext from "../../context/auth.context";
 
 class BookingsPage extends Component {
+  state = {
+    isLoading: false,
+    bookings: [],
+  };
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    this.getBookings();
+  }
+
+  getBookings = () => {
+    this.setState({
+      isLoading: true,
+    });
+    const requestBody = {
+      query: `
+          query {
+            bookings {
+              _id
+              event {
+                title
+                date
+              }
+              createdAt
+              updatedAt
+            }
+          }
+        `,
+    };
+
+    const token = this.context.token;
+
+    fetch("http://localhost:5000/home", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        this.setState({
+          bookings: resData.data.bookings,
+          isLoading: false,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
-    return <h2>Bookings Page</h2>;
+    const bookingsList = this.state.bookings.map((booking) => {
+      return (
+        <li key={booking._id}>
+          {booking.event.title} -{" "}
+          {new Date(booking.event.date).toLocaleDateString()}
+        </li>
+      );
+    });
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? <Spinner /> : <ul>{bookingsList}</ul>}
+      </React.Fragment>
+    );
   }
 }
 
--- frontend/src/pages/Events/events.js
+++ frontend/src/pages/Events/events.js
@@ -16,6 +16,8 @@ class EventsPage extends Component {
     isLoading: false,
   };
 
+  isActive = true;
+
   static contextType = AuthContext;
 
   constructor(props) {
@@ -78,16 +80,19 @@ class EventsPage extends Component {
         return res.json();
       })
       .then((resData) => {
-        this.setState({
-          events: resData.data.events,
-          isLoading: false,
-        });
+        if (this.isActive) {
+          this.setState({
+            events: resData.data.events,
+            isLoading: false,
+          });
+        }
       })
       .catch((err) => {
-        console.log(err);
-        this.setState({
-          isLoading: false,
-        });
+        if (this.isActive) {
+          this.setState({
+            isLoading: false,
+          });
+        }
       });
   };
 
@@ -166,7 +171,47 @@ class EventsPage extends Component {
       });
   };
 
-  bookEventHandler = () => {};
+  bookEventHandler = () => {
+    const requestBody = {
+      query: `
+          mutation {
+            bookEvent(eventId: "${this.state.selectedEvent._id}") {
+              _id
+              createdAt
+              updatedAt
+            }
+          }
+        `,
+    };
+
+    const token = this.context.token;
+
+    fetch("http://localhost:5000/home", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        this.setState({
+          selectedEvent: null,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          selectedEvent: null,
+        });
+      });
+  };
 
   onViewDetail = (eventId) => {
     this.setState((prevState) => {
@@ -176,6 +221,10 @@ class EventsPage extends Component {
       };
     });
   };
+
+  componentWillUnmount() {
+    this.isActive = false;
+  }
 
   render() {
     return (
@@ -219,7 +268,7 @@ class EventsPage extends Component {
           <Modal
             title="Event Details"
             canCancel
-            canConfirm
+            canConfirm={this.context.token ? true : false}
             confirmText="Book"
             cancelText="Back"
             onCancel={this.cancelHandler}
