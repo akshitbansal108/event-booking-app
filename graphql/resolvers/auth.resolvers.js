3b3d880e26b42ef042b4af5110a85ef299373805
akshitbansal-bst <akakshitbansal@gmail.com>
2020-10-06 21:55:14 +0530

    auto login on signup

--- graphql/resolvers/auth.resolvers.js
+++ graphql/resolvers/auth.resolvers.js
@@ -3,6 +3,31 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../../models/user");
 
+const loginCheck = async (email, password) => {
+  try {
+    const userData = await User.findOne({ email: email });
+    if (!userData) {
+      throw new Error("Invalid Credentials");
+    }
+
+    const passwordVerified = await bcrypt.compare(password, userData.password);
+    if (!passwordVerified) {
+      throw new Error("Invalid Credentials");
+    }
+
+    const token = jwt.sign(
+      { userId: userData.id, email: userData.email },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+    return { userId: userData.id, token: token, tokenExpiration: 1 };
+  } catch (err) {
+    throw err;
+  }
+};
+
 module.exports = {
   createUser: async (args) => {
     try {
@@ -16,36 +41,12 @@ module.exports = {
         password: hashedPassword,
       });
       const newUser = await user.save();
-      return { ...newUser._doc, _id: user.id, password: null };
+      return await loginCheck(args.userInput.email, args.userInput.password);
     } catch (err) {
       throw err;
     }
   },
   login: async ({ email, password }) => {
-    try {
-      const userData = await User.findOne({ email: email });
-      if (!userData) {
-        throw new Error("Invalid Credentials");
-      }
-
-      const passwordVerified = await bcrypt.compare(
-        password,
-        userData.password
-      );
-      if (!passwordVerified) {
-        throw new Error("Invalid Credentials");
-      }
-
-      const token = jwt.sign(
-        { userId: userData.id, email: userData.email },
-        process.env.SECRET_KEY,
-        {
-          expiresIn: "1h",
-        }
-      );
-      return { userId: userData.id, token: token, tokenExpiration: 1 };
-    } catch (err) {
-      throw err;
-    }
+    return await loginCheck(email, password);
   },
 };
